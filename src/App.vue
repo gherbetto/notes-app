be9e1bd54e0a1d5629fafca08276f82f42524762
Dmitrii Gherastovschi <dgherastovschi@icloud.com>
2023-07-03 01:05:19 +0500

    moved to vue cli, initialized the project, moved the message part to component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,89 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="wrapper">
+        <!--      header-->
+        <div class="wrapper-content">
+            <section>
+                <div class="container">
+                    <h1>{{ title }}</h1>
+
+                    <message v-if="message" :message="message"></message>
+
+                    <div class="new-note">
+                        <input v-model="note.title" type="text">
+                        <textarea v-model="note.descr"></textarea>
+                        <button @click="addNote" >New Note</button>
+                    </div>
+                    <!--    note list   -->
+                    <div class="note" v-for="(note, index) in notes" :key="index">
+                        <div class="note-header">
+                            <p>{{ note.title }}</p>
+                        </div>
+                        <div class="note-body">
+                            <p>{{ note.descr }}</p>
+                            <span>{{ note.date }}</span>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+        <!--      footer-->
+    </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import message from '@/components/Message.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    components: {
+        message
+    },
+    data () {
+        return {
+            title: 'Notes App',
+            message: null,
+            note: {
+                title: '',
+                descr: '',
+            },
+            notes: [
+                {
+                    title: 'First Note',
+                    descr: 'Description for the first note',
+                    date: new Date(Date.now()).toLocaleString()
+                },
+                {
+                    title: 'Second Note',
+                    descr: 'Description for the second note',
+                    date: new Date(Date.now()).toLocaleString()
+                },
+                {
+                    title: 'Third Note',
+                    descr: 'Description for the third note',
+                    date: new Date(Date.now()).toLocaleString()
+                },
+            ]
+        }
+    },
+    methods: {
+        addNote () {
+            let {title, descr} = this.note
+            if (title === '') {
+                this.message = 'title can`t be blank!'
+                return false
+            }
+            this.notes.push({
+                title,
+                descr,
+                date: new Date(Date.now()).toLocaleString(),
+            })
+            this.message = null
+            this.note.title = ''
+            this.note.descr = ''
+        }
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
